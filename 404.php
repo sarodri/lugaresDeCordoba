d284cf66f4f36c6dc29e4a62553eb80edad11ba4
sarodri <sandra.ropero1987@gmail.com>
2023-03-31 10:14:37 +0200

    Page 404

--- 404.php
+++ 404.php
@@ -1,11 +1,13 @@
 <?php get_header()?>
 
-<div class='container my-3'id="page404" >
-    <div class="page404 d-flex justify-content-center" >
-        <h1>Error 404 página no encontrada</h1>
-        <h2>Haz <a href="<?php echo home_url();?>">click aquí </a> para volver a la página rincipal</h2>
+<main class="container d-flex justify-content-center"> 
+    <div class="error-404  align-items-center ">
+        <img src="<?php echo get_template_directory_uri()?>/assets/img/fondo.jpg" alt="fondo">
+            <h1 id="error404">¡Vaya! Parece que esta página no existe</h1>
+            <p id="error404">Puedes volver a la página principal dándole a <a href="<?php echo home_url(); ?>">éste enlace</a> :)</p>
+        
+        
     </div>
-</div>
-
+</main> 
 
 <?php get_footer()?>
