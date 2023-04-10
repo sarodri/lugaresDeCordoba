e56caf291b970851a640b86f9cda8e6161383608
sarodri <sandra.ropero1987@gmail.com>
2023-04-10 10:29:19 +0200

    404

--- 404.php
+++ 404.php
@@ -3,11 +3,12 @@
 <main class="container d-flex justify-content-center"> 
     <div class="error-404  align-items-center ">
         <img src="<?php echo get_template_directory_uri()?>/assets/img/fondo.jpg" alt="fondo">
-            <h1 id="error404">¡Vaya! Parece que esta página no existe</h1>
-            <p id="error404">Puedes volver a la página principal dándole a <a href="<?php echo home_url(); ?>">éste enlace</a> :)</p>
+            <h1 class="error404">¡Vaya! Parece que esta página no existe</h1>
+            <p class="error404">Puedes volver a la página principal dándole a <a href="<?php echo home_url(); ?>">éste enlace</a> :)</p>
         
         
     </div>
 </main> 
 
 <?php get_footer()?>
+
